f4a435ceb6c775df3b6708db15c69b79c10b3b3d
jamieyu0914 <jamieyu0914@gmail.com>
2024-03-07 15:27:47 +0800

    Type and varibles 	new file:   ConsoleApp1/ConsoleApp1/Program_type_varibles.cs

---
+++ ConsoleApp1/ConsoleApp1/Program_type_varibles.cs
@@ -0,0 +1,27 @@
+﻿//常見資料型態 & 變數
+
+//字串 string "小白好帥"
+//字元 char'a'
+//整數 int 160
+//浮點數 double 160.5
+//布林值 bool true false
+
+string name = "小白";
+
+char sex = 'M';
+
+int age = 30;
+
+double height = 180.3;
+
+bool is_male = true;
+
+System.Console.WriteLine("有一個人叫" + name);
+System.Console.WriteLine(name + "今年" + age + "歲");
+System.Console.WriteLine(name + "身高" + height + "公分");
+
+
+
+
+
+

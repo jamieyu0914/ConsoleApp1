7f401f97f2f5662e21c2300ca15dd48017a367de
jamieyu0914 <jamieyu0914@gmail.com>
2024-03-11 16:59:55 +0800

    Int and Double 	new file:   ConsoleApp1/ConsoleApp1/Program_int_and_double.cs

---
+++ ConsoleApp1/ConsoleApp1/Program_int_and_double.cs
@@ -0,0 +1,41 @@
+﻿//數字的常見用法 (整數 浮點數)
+
+System.Console.WriteLine(5 + 2);
+System.Console.WriteLine(5 - 2);
+System.Console.WriteLine(5 * 2);
+System.Console.WriteLine(5 / 2);
+
+//除法 浮點數運算
+System.Console.WriteLine(5 / 2);
+System.Console.WriteLine(5 / 2.0);
+
+//連續運算 先乘除後加減
+System.Console.WriteLine(5+2*3);
+System.Console.WriteLine((5+2)*3);
+
+int num = 3;
+System.Console.WriteLine((5 + 2) * num);
+
+//絕對值 運算
+System.Console.WriteLine(System.Math.Abs(-10));
+System.Console.WriteLine(System.Math.Abs(10));
+
+//次方 運算
+System.Console.WriteLine(System.Math.Pow(2, 3));
+System.Console.WriteLine(System.Math.Pow(4, 3));
+
+//開根號 運算
+System.Console.WriteLine(System.Math.Sqrt(64));
+System.Console.WriteLine(System.Math.Sqrt(36));
+
+//最大值 運算
+System.Console.WriteLine(System.Math.Max(2, 100));
+System.Console.WriteLine(System.Math.Max(101, 100));
+
+//最小值 運算
+System.Console.WriteLine(System.Math.Min(2, 100));
+System.Console.WriteLine(System.Math.Min(101, 100));
+
+//四捨五入 運算
+System.Console.WriteLine(System.Math.Round(3.4));
+System.Console.WriteLine(System.Math.Round(3.6));

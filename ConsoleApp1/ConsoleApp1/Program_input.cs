2f112502b3d99f65ba49abebab5881119e1bcda3
jamieyu0914 <jamieyu0914@gmail.com>
2024-03-11 17:18:09 +0800

    Input 	new file:   ConsoleApp1/ConsoleApp1/Program_input.cs

---
+++ ConsoleApp1/ConsoleApp1/Program_input.cs
@@ -0,0 +1,15 @@
+﻿//取得用戶輸入
+
+System.Console.Write("請輸入您的名字:"); //不換行
+System.Console.ReadLine();
+
+
+//使用變數 存取輸入值
+System.Console.Write("請輸入您的名字:"); //不換行
+string name = System.Console.ReadLine();
+System.Console.Write("請輸入您的年紀:"); //不換行
+string age = System.Console.ReadLine();
+System.Console.WriteLine("你好啊" + name + "你今年" + age + "歲");
+
+
+
